cfcd41c7b75f5a7289bd0a1629bf1831b73f49ba
Adryel Santos <118002541+AdryelJr@users.noreply.github.com>
2023-08-17 22:19:12 -0300

    code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,75 +7,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('mensagem do sininho')));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Go to the next page',
-            onPressed: () {
-              Navigator.push(context,MaterialPageRoute<void>(
-                  builder: (BuildContext context) {
-                    return Scaffold(
-                      appBar: AppBar(
-                        title: const Text('Voltar Página'),
-                        actions: <Widget>[
-                          IconButton(
-                            icon: const Icon(Icons.navigate_next),
-                            tooltip: 'Go to the next page',
-                            onPressed: () {
-                              Navigator.push(context, MaterialPageRoute<void>(
-                                  builder: (BuildContext context) {
-                                    return Scaffold(
-                                      appBar: AppBar(
-                                        title: const Text('Voltar Página'),
-                                      ),
-                                      body: const Center(
-                                        child: Text(
-                                          'Terceira página',
-                                          style: TextStyle(fontSize: 24),
-                                        ),
-                                      ),
-                                      floatingActionButton:
-                                          FloatingActionButton(
-                                              child: Text('Botão pagina 3'),
-                                              onPressed: () {
-                                                print('botão pagina 3');
-                                              }),
-                                    );
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      body: const Center(
-                        child: Text(
-                          'Segunda página',
-                          style: TextStyle(fontSize: 24),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              );
-            },
-          ),
-        ],
+        title: Text('Tarefas'),
       ),
-      body: Center(
-        child: Text(
-          'Meu aplicativo',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            height: 150,
+            width: 1000,
+            color: Colors.green,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
