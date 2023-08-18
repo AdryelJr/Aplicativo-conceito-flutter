1b711d2a4af16aca64554695e2ad9ce13e77ac84
Adryel Santos <118002541+AdryelJr@users.noreply.github.com>
2023-08-18 17:42:23 -0300

    code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,57 @@
 import 'package:flutter/material.dart';
 
-import 'myApp.dart';
-
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Cards();
+  }
+}
+
+class Cards extends StatelessWidget {
+  const Cards({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tarefas'),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            height: 150,
+            width: 1000,
+            color: Color.fromARGB(255, 127, 1, 149),
+          ),
+          Container(
+            height: 100,
+            width: 1000,
+            color: Color.fromARGB(255, 224, 224, 224),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(height: 70,width: 70, color: Color.fromARGB(255, 127, 1, 149),),
+                Text("Primeira tarefa"),
+                ElevatedButton(onPressed:(){}, child: Icon(Icons.add))
+              ]
+              ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Botão página 1');
+        },
+      ),
+    );
+  }
 }
