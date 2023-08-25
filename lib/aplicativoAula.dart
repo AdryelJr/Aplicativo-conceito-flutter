21a7c9609e19c5f7e3490bf008df8c8523c446ae
Adryel Santos <118002541+AdryelJr@users.noreply.github.com>
2023-08-24 21:56:00 -0300

    Create aplicativoAula.dart

---
+++ lib/aplicativoAula.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_flutter/botao1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu aplicativo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Barra de navigator'),
+        ),
+        body: ListView(
+          children: [
+            Tarefa(texto: "Jogar"),
+            Tarefa(texto: "Estudar"),
+            Tarefa(texto: "Programar")
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+class Tarefa extends StatelessWidget {
+  const Tarefa ({super.key, required this.texto});
+
+  final String texto;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 150,
+          color: Colors.blue,
+        ),
+        Container(
+          height: 100,
+          color: Colors.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.white,
+              ),
+              Text(texto),
+              ElevatedButton(
+                  onPressed: () {}, child: Icon(Icons.keyboard_double_arrow_up))
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
